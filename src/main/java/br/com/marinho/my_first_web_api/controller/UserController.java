2146e295823b378514514e9eaa2ceba74e8732f0
cmarinho-dev <cmarinho.dev@gmail.com>
2025-08-16 15:52:39 -0300

    Initial commit

---
+++ src/main/java/br/com/marinho/my_first_web_api/controller/UserController.java
@@ -0,0 +1,36 @@
+package br.com.marinho.my_first_web_api.controller;
+
+import br.com.marinho.my_first_web_api.model.User;
+import br.com.marinho.my_first_web_api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping()
+    public List<User> getUsers() {
+       return repository.findAll();
+    }
+
+    @GetMapping("/{username}")
+    public User getOne(@PathVariable("username") String username) {
+        return repository.findByUserName(username);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id) {
+        repository.deleteById(id);
+    }
+
+    @PostMapping()
+    public void postUser(@RequestBody User user) {
+        repository.save(user);
+    }
+}
---
+++ src/main/java/br/com/marinho/my_first_web_api/repository/UserRepository.java
@@ -0,0 +1,39 @@
+package br.com.marinho.my_first_web_api.repository;
+
+import br.com.marinho.my_first_web_api.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserRepository {
+    public void save(User user) {
+        if (user.getId()==null)
+            System.out.printf("SAVE - Receiving user '%s' at repository layer", user);
+        else
+            System.out.println("UPDATE - Receiving user at repository layer");
+    }
+
+    public void deleteById(Integer id) {
+        System.out.printf("DELETE/id - Getting the id '%d' to delete a user%n", id);
+    }
+
+    public List<User> findAll() {
+        System.out.println("LIST - Showing all users");
+        List<User> users = new ArrayList<>();
+        users.add(new User("admin","password"));
+        users.add(new User("marinho","masterpass"));
+        return users;
+    }
+
+    public User findById(Integer id) {
+        System.out.printf("FIND/id - Getting the id '%d' to locate a user%n", id);
+        return new User("marinho", "masterpass");
+    }
+
+    public User findByUserName(String username) {
+        System.out.printf("FIND/username - Receiving the username '%s' to locate a user%n", username);
+        return new User("marinho", "masterpass");
+    }
+}
